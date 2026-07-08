c66c79b03900ee43208f8bfaac8d81b259f602aa
jpgcconsultores-tech <jpgc.consultores@gmail.com>
2026-07-08 12:36:58 -0500

    Update streamlit_app.py
    se adicionó la conexión cnx

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 #st.write("You favorite fruit is:", option)
 
-session = get_active_session()
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
